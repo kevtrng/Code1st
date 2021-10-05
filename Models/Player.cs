894c792697e6ab023fa2183e9a3b12847118389f
Kev Truong <kevtrng00@gmail.com>
2021-10-05 11:16:54 -0700

    first commit

---
+++ Models/Player.cs
@@ -0,0 +1,16 @@
+namespace Code1st.Models
+{
+    public class Player
+    {
+        public int PlayerId {get; set;} 
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+
+        public string TeamName { get; set; }
+
+        public Team Team { get; set; }
+        public string Position { get; internal set; }
+    }
+
+    
+}
---
+++ Models/Team.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Code1st.Models
+{
+    public class Team
+    {
+        [Key]
+        public string TeamName {get; set;}
+        public string City {get; set;}
+
+        public IEnumerable<Player> Players { get; set; }
+
+    }
+}
